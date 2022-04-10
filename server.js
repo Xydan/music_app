ca6f129772377d8d8ec685007e46de8b7cd5d982
andy li <andym.li1995@gmail.com>
2022-04-10 07:59:45 -0400

    trial #4

--- server.js
+++ server.js
@@ -27,27 +27,23 @@ userService.connect()
     process.exit();
 });
 
-app.post('/api/user/register', (req, res)=>{ 
-    userService.registerUser(req.body).then(result => res.send(result)).catch(error => res.send(error));
-});
+// app.post('/api/user/register', (req, res)=>{ 
+//     userService.registerUser(req.body).then(result => res.send(result)).catch(error => res.send(error));
+// });
 
-app.post('/api/user/login', (req, res)=>{ 
-    userService.checkUser(req.body).then(user => jwt.sign({ _id: user._id, userName: user.userName}, process.env.JWT_SECRET))
-        .then(result => res.send({message: "login successful", token: result })).catch(error => res.status(422).send(error));
-});
+// app.post('/api/user/login', (req, res)=>{ 
+//     userService.checkUser(req.body).then(user => jwt.sign({ _id: user._id, userName: user.userName}, process.env.JWT_SECRET))
+//         .then(result => res.send({message: "login successful", token: result })).catch(error => res.status(422).send(error));
+// });
 
-app.get('/api/user/favourites', passport.authenticate('jwt', {session: false}),(req, res)=>{
-    userService.getFavourites(req.user._id).then(result => res.send(result)).catch(error => res.send(error));
-});
+// app.get('/api/user/favourites', passport.authenticate('jwt', {session: false}),(req, res)=>{
+//     userService.getFavourites(req.user._id).then(result => res.send(result)).catch(error => res.send(error));
+// });
 
-app.put('/api/user/favourites/:id', passport.authenticate('jwt', {session: false}), (req, res)=>{
-    userService.addFavourite(req.user._id, req.params.id).then(result => res.send(result)).catch(error => res.send(error));
-});
+// app.put('/api/user/favourites/:id', passport.authenticate('jwt', {session: false}), (req, res)=>{
+//     userService.addFavourite(req.user._id, req.params.id).then(result => res.send(result)).catch(error => res.send(error));
+// });
 
-app.delete('/api/user/favourites/:id', passport.authenticate('jwt', {session: false}), (req, res)=>{
-    userService.removeFavourite(req.user._id, req.params.id).then(result => res.send(result)).catch(error => res.send(error));
-});
-
-app.get('/', (req, res)=>{
-    res.sendFile({message: "Hey!"});
-});
+// app.delete('/api/user/favourites/:id', passport.authenticate('jwt', {session: false}), (req, res)=>{
+//     userService.removeFavourite(req.user._id, req.params.id).then(result => res.send(result)).catch(error => res.send(error));
+// });
